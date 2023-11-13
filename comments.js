a3e43f3a0f2870a547ff45e21fbcaf3f9c25a7c4
Wale Kujore <adewalekujore6@gmail.com>
2023-11-13 13:45:07 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -3,9 +3,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
-// load comments module
-const comments = require('./comments.js');
-
 // configure body-parser
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -15,13 +12,12 @@ const port = 3000;
 
 // handle GET request
 app.get('/comments', (req, res) => {
-  res.json(comments.getComments());
+  res.json("comments");
 });
 
 // handle POST request
 app.post('/comments', (req, res) => {
   const newComment = req.body;
-  comments.addComment(newComment);
   res.json(newComment);
 });
 
@@ -29,14 +25,12 @@ app.post('/comments', (req, res) => {
 app.put('/comments/:id', (req, res) => {
   const id = req.params.id;
   const updatedComment = req.body;
-  comments.updateComment(id, updatedComment);
   res.json(updatedComment);
 });
 
 // handle DELETE request
 app.delete('/comments/:id', (req, res) => {
   const id = req.params.id;
-  comments.deleteComment(id);
   res.send('Comment deleted');
 });
 
