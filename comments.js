8b49ba3095cb8c3d2cc72c2a7e6f74753ed3f180
Wale Kujore <adewalekujore6@gmail.com>
2023-11-13 13:40:58 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,69 +1,46 @@
-// create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var urlencodedParser = bodyParser.urlencoded({ extended: false });
-// set up handlebars view engine
-var handlebars = require('express-handlebars')
- .create({ defaultLayout: 'main' });
-app.engine('handlebars', handlebars.engine);
-app.set('view engine', 'handlebars');
+// create web server with express
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
-// set up mongo db
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/";
+// load comments module
+const comments = require('./comments.js');
 
-app.set('port', process.env.PORT || 3000);
+// configure body-parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-// set up static file
-app.use(express.static(__dirname + '/public'));
+// set port
+const port = 3000;
 
-// set up routes
-app.get('/', function(req, res) {
- res.render('home');
+// handle GET request
+app.get('/comments', (req, res) => {
+  res.json(comments.getComments());
 });
 
-app.get('/about', function(req, res) {
- res.render('about');
+// handle POST request
+app.post('/comments', (req, res) => {
+  const newComment = req.body;
+  comments.addComment(newComment);
+  res.json(newComment);
 });
 
-app.get('/contact', function(req, res) {
- res.render('contact');
+// handle PUT request
+app.put('/comments/:id', (req, res) => {
+  const id = req.params.id;
+  const updatedComment = req.body;
+  comments.updateComment(id, updatedComment);
+  res.json(updatedComment);
 });
 
-app.get('/thankyou', function(req, res) {
- res.render('thankyou');
+// handle DELETE request
+app.delete('/comments/:id', (req, res) => {
+  const id = req.params.id;
+  comments.deleteComment(id);
+  res.send('Comment deleted');
 });
 
-app.post('/contact', urlencodedParser, function(req, res) {
- console.log(req.body);
- MongoClient.connect(url, function(err, db) {
- if (err) throw err;
- var dbo = db.db("contact");
- var myobj = { name: req.body.name, email: req.body.email, subject: req.body.subject, message: req.body.message };
- dbo.collection("contact").insertOne(myobj, function(err, res) {
- if (err) throw err;
- console.log("1 document inserted");
- db.close();
- });
- });
- res.render('thankyou', { contact: req.body });
-});
-
-// custom 404 page
-app.use(function(req, res) {
- res.status(404);
- res.render('404');
-});
-
-// custom 500 page
-app.use(function(err, req, res, next) {
- console.error(err.stack);
- res.status(500);
- res.render('500');
-});
-
-app.listen(app.get('port'), function() {
- console.log('Express started on http://localhost:' +
-app.get('port') + '; press Ctrl-C to terminate.');
+// start server
+app.listen(port, () => {
+  console.log(`Server started: Listening at port ${port}`);
 });
